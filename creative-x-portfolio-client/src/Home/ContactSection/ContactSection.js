e0d6a8c39ba45d3fb93d73195b5eab5aa55426ae
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2021-12-04 21:56:54 -0800

    static init contact

--- creative-x-portfolio-client/src/Home/ContactSection/ContactSection.js
+++ creative-x-portfolio-client/src/Home/ContactSection/ContactSection.js
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom"
+import "./ContactSection.css"
+
 export default function ContactSection() {
   return (
-    <div></div>
+    <div className="section get-in-touch">
+      <div className="container-medium">
+        <div className="text-center">
+          <h2>Get In Touch</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit lacus nisi, erat sed porta. Sem bibendum eu dui convallis. Et id.</p>
+          <div className="mg-top-32px">
+            <Link to="/contact" className="button-primary w-button">
+              Send Me A Message
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="text-big-cta top">Contact Me</div>
+      <div className="text-big-cta bottom">Contact Me</div>
+    </div>
   )
 }
