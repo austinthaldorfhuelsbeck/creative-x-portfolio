c2b153fae72d17cce72a1d552283691ee9609aa9
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2021-12-04 17:53:29 -0800

    reactify homehero components

---
+++ creative-x-portfolio-client/src/NavBar/NavBar.js
@@ -0,0 +1,36 @@
+import "./NavBar.css"
+
+export default function NavBar() {
+  const navItems = [
+    {
+      title: "Home",
+      link: "#",
+    },
+    {
+      title: "Portfolio",
+      link: "#portfolio",
+    },
+    {
+      title: "About",
+      link: "#about",
+    },
+    {
+      title: "Contact",
+      link: "#contact",
+    }
+  ]
+
+  return (
+    <div className="container-default w-container">
+      <div className="header-wrapper">
+        <ul className="nav-bar">
+          {navItems.map((item) => (
+            <li key={item.title} className="nav-item">
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
